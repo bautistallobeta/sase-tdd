75072bb67a718cac1642c78476450506f82b8ac7
bautistallobeta <bautillobeta@gmail.com>
2024-08-06 15:49:09 -0300

    Codigo comentado y refactoreado

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -4,82 +4,110 @@
 #include "clock.h"
 #include "unity.h"
 
+/* Tamano del array de tiempo ( ej: si es 6, entonces [H, H, M, M, S, S]) */
 #define TIME_SIZE 6
+/* Ticks por segundo que recibe el clock */
 #define TICKS_PER_SECOND 5
+/* Ticks por minuto */
 #define TICKS_PER_MINUTE TICKS_PER_SECOND * 60
+/* Ticks por hora */
 #define TICKS_PER_HOUR TICKS_PER_MINUTE * 60
+/* Ticks por dia */
 #define TICKS_PER_DAY TICKS_PER_HOUR * 24
 
+/* Puntero a instancia de reloj */
 clocks_t reloj;
+/* Variable de estado de activacion de la alarma (true = sonando ; false = apagada) */
 static bool alarmaActivada;
 
-void simulateTicks(uint32_t tiks) {
-    for (int i = 0; i < tiks; i++) {
+/* Funcion auxiliar para simular el paso de n ticks */
+void simulateTicks(uint32_t ticks) {
+    for (int i = 0; i < ticks; i++) {
         newTick(reloj);
     }
 }
 
+/* Funcion que llama el reloj para indicar el encendido/apagado de la alarma */
 void alarmEvent(clocks_t reloj, bool state) {
     alarmaActivada = state;
 }
 
+/* Funcion de setup para inicializar el reloj en un estado predeterminado para las pruebas */
 void setUp() {
     static const uint8_t INICIAL[] = {1, 2, 3, 4, 0, 0};
 
     alarmaActivada = false;
 
     reloj = createClock(TICKS_PER_SECOND, alarmEvent);
+
     setTime(reloj, INICIAL, sizeof(INICIAL));
 }
 
 /* Test de inicializacion del reloj */
 void test_start_up(void) {
-    static const uint8_t ESPERADO[] = {0, 0, 0, 0, 0, 0}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /* Almacenará la hora que retorna la funcion getTime*/
+    /*Array con la hora esperada*/
+    static const uint8_t ESPERADO[] = {0, 0, 0, 0, 0, 0};
+    /* Variable para almacenar la hora seteada*/
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
-    reloj = createClock(TICKS_PER_SECOND, alarmEvent); /* Inicializacion del reloj: Recibe por parametro cuantos ticks por segundo recibe el reloj*/
+    /* Inicializacion del reloj*/
+    reloj = createClock(TICKS_PER_SECOND, alarmEvent);
 
-    TEST_ASSERT_FALSE(getTime(reloj, hora, TIME_SIZE));       /* Test de la validez de la hora*/
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE); /* Test del resultado de getTime */
-
-    TEST_ASSERT_FALSE(getAlarm(reloj, hora, TIME_SIZE));
+    /* Test de la validez de la hora*/
+    TEST_ASSERT_FALSE(getTime(reloj, hora, TIME_SIZE));
+    /* Test del resultado de getTime */
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
 /* Test de seteo de hora */
 void test_set_time(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 0, 0}; /*Array que representa la hora esperada*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 0, 0};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
-    uint8_t hora[TIME_SIZE] = {0xFF}; /* Almacenará la hora que retorna la funcion getTime*/
-
+    /* Test de validez de la hora */
     TEST_ASSERT_TRUE(getTime(reloj, hora, TIME_SIZE));
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE); /* Test del resultado de getTime */
+    /* Test del resultado de getTime */
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de un segundo */
 void test_one_second(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 0, 1}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /*Almacenará la hora que retorna la funcion getTime*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 0, 1};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
+    /* Simulacion de un segundo */
     simulateTicks(TICKS_PER_SECOND);
 
     getTime(reloj, hora, TIME_SIZE);
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE); /* Test del resultado de getTime */
+    /* Test del resultado de getTime */
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de 10 segundos */
 void test_ten_seconds(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 1, 0}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /*Almacenará la hora que retorna la funcion getTime*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 1, 0};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     simulateTicks(TICKS_PER_SECOND * 10);
 
     getTime(reloj, hora, TIME_SIZE);
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE); /* Test del resultado de getTime */
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de 1 minuto */
 void test_one_minute(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 3, 5, 0, 0}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /*Almacenará la hora que retorna la funcion getTime*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 3, 5, 0, 0};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     simulateTicks(TICKS_PER_MINUTE);
 
@@ -88,9 +116,12 @@ void test_one_minute(void) {
     TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de 10 minutos */
 void test_ten_minutes(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 4, 4, 0, 0}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /*Almacenará la hora que retorna la funcion getTime*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 4, 4, 0, 0};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     simulateTicks(TICKS_PER_MINUTE * 10);
 
@@ -99,9 +130,12 @@ void test_ten_minutes(void) {
     TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de 1 hora */
 void test_one_hour(void) {
-    static const uint8_t ESPERADO[] = {1, 3, 3, 4, 0, 0}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /*Almacenará la hora que retorna la funcion getTime*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {1, 3, 3, 4, 0, 0};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     simulateTicks(TICKS_PER_HOUR);
 
@@ -110,9 +144,12 @@ void test_one_hour(void) {
     TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de 10 horas */
 void test_ten_hours(void) {
-    static const uint8_t ESPERADO[] = {2, 2, 3, 4, 0, 0}; /*Array que representa la hora esperada*/
-    uint8_t hora[TIME_SIZE] = {0xFF};                     /*Almacenará la hora que retorna la funcion getTime*/
+    /*Array que con la hora esperada*/
+    static const uint8_t ESPERADO[] = {2, 2, 3, 4, 0, 0};
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     simulateTicks(TICKS_PER_HOUR * 10);
 
@@ -121,9 +158,13 @@ void test_ten_hours(void) {
     TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
+/* Test del paso de 1 dia */
 void test_one_day(void) {
+    /*Array que con la hora esperada*/
     static const uint8_t ESPERADO[] = {1, 2, 3, 4, 0, 0};
-    uint8_t hora[TIME_SIZE] = {0xFF}; /* Almacenará la hora que retorna la funcion getTime*/
+    /* Variable parar almacenar la hora seteada */
+    uint8_t hora[TIME_SIZE] = {0xFF};
+
     simulateTicks(TICKS_PER_DAY);
 
     getTime(reloj, hora, TIME_SIZE);
@@ -133,45 +174,34 @@ void test_one_day(void) {
 
 /* Test de seteo y activacion de alarma*/
 void test_set_alarm(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
-
-    uint8_t hora[TIME_SIZE] = {0xFF}; /* Almacenará la hora que retorna la funcion getAlarm*/
+    /*Array que con la alarma esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 4, 0, 0, 0};
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     setAlarm(reloj, ESPERADO, TIME_SIZE);
 
     TEST_ASSERT_TRUE(getAlarm(reloj, hora, TIME_SIZE));
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE); /* Test del resultado de getAlarm */
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, TIME_SIZE);
 }
 
-/* Test de desactivacion de alarma (ACLARACION: no es el apagado o pospuesta de la alarma)*/
+/* Test de desactivacion de alarma (ACLARACION: no es el apagado o posposición de la alarma)*/
 void test_desactivate_alarm(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 4, 0, 0, 5}; /*Array que representa la alarma esperada*/
+    /*Array que con la alarma esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 4, 0, 0, 5};
 
-    uint8_t hora[TIME_SIZE] = {0xFF}; /* Almacenará la hora que retorna la funcion getAlarm*/
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     setAlarm(reloj, ESPERADO, TIME_SIZE);
-
-    TEST_ASSERT_TRUE(getAlarm(reloj, hora, TIME_SIZE));
 
     desactivateAlarm(reloj);
 
     TEST_ASSERT_FALSE(getAlarm(reloj, hora, TIME_SIZE));
 }
 
-/*Test de alarma activada pero aun no se llego a la hora (aun no suena)*/
-void test_alarm_activated_off(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
-
-    setAlarm(reloj, ALARMA, TIME_SIZE);
-
-    simulateTicks(TICKS_PER_MINUTE * 4);
-
-    TEST_ASSERT_FALSE(alarmaActivada);
-}
-
-/*Test de alarma activada cuando se llega a la hora seteada de la alarma (debe empezar a sonar)*/
+/*Test de alarma activada, no suena hasta llegar la hora seteada*/
 void test_alarm_activated_on(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
+    /*Array que con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
@@ -182,38 +212,41 @@ void test_alarm_activated_on(void) {
     TEST_ASSERT_TRUE(alarmaActivada);
 }
 
-/*Test de alarma, cuando se llega a la hora seteada, pero la alarma esta desactivada*/
+/*Test de alarma, el reloj llega a la hora seteada pero la alarma esta desactivada*/
 void test_alarm_desactivated_off(void) {
-    static const uint8_t ESPERADO[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
+    /*Array que con la alarma esperada*/
+    static const uint8_t ESPERADO[] = {1, 2, 4, 0, 0, 0};
 
-    uint8_t hora[TIME_SIZE] = {0xFF}; /* Almacenará la hora que retorna la funcion getAlarm*/
+    uint8_t hora[TIME_SIZE] = {0xFF};
 
     setAlarm(reloj, ESPERADO, TIME_SIZE);
 
-    TEST_ASSERT_TRUE(getAlarm(reloj, hora, TIME_SIZE));
-
     desactivateAlarm(reloj);
+
     simulateTicks(TICKS_PER_MINUTE * 7);
 
     TEST_ASSERT_FALSE(alarmaActivada);
 }
 
-/*Test de alarma, la alarma suena, pasan 24hs y no se paro ni pospuso (debe seguir sonando)*/
+/*Test de alarma, la alarma suena y pasan 24hs sin pararse ni posponerse (debe seguir sonando)*/
 void test_alarm_already_on(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
+    /*Array que con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
-    simulateTicks(TICKS_PER_MINUTE * 6); /*Suena la alarma*/
+    /*Suena la alarma*/
+    simulateTicks(TICKS_PER_MINUTE * 6);
+    /*Pasan 24hs*/
+    simulateTicks(TICKS_PER_DAY);
+    /*Test para comprobar si sigue sonando*/
     TEST_ASSERT_TRUE(alarmaActivada);
-
-    simulateTicks(TICKS_PER_DAY);     /*Pasab 24hs*/
-    TEST_ASSERT_TRUE(alarmaActivada); /*Sigue sonando*/
 }
 
-/* Test de apagado de la alarma cuando esta sonando (ACLARACIÓN: no es la desactivacion de la alarma) */
+/* Test de alarma, se apaga la alarma cuando SI esta sonando (ACLARACIÓN: no es la desactivacion de la alarma) */
 void test_alarm_shut(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
+    /*Array que con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
@@ -222,16 +255,15 @@ void test_alarm_shut(void) {
     TEST_ASSERT_FALSE(alarmaActivada);
 }
 
-/*Test de apagado de la alarma cuando no esta sonando (no debe suceder nada)*/
+/*Test de alarma, se apaga de la alarma cuando NO esta sonando (no debe suceder nada)*/
 void test_alarm_shut_when_off(void) {
-    TEST_ASSERT_FALSE(alarmaActivada);
     shutAlarm(reloj, TIME_SIZE);
     TEST_ASSERT_FALSE(alarmaActivada);
 }
 
-/* Test de posposicion de alarma cuando esta sonando*/
+/* Test de posposicion de alarma cuando SI esta sonando*/
 void test_alarm_postponed(void) {
-    /*Array que representa la alarma esperada*/
+    /*Array que con la alarma esperada*/
     static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
     /* Constante con la cantidad de minutos que se desea posponer */
     static const uint8_t N_MINUTOS = 5;
@@ -245,10 +277,22 @@ void test_alarm_postponed(void) {
     TEST_ASSERT_FALSE(alarmaActivada);
 }
 
-/* Test, la alarma es pospuesta n minutos, y no suena hasta pasar n minutos*/
+/* Test de alarma, se pospone pero no estaba sonando (no deberia ocurrir nada)*/
+void test_alarm_postponed_off(void) {
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t N_MINUTOS = 5;
+
+    postponeAlarm(reloj, TIME_SIZE, TIME_SIZE);
+    simulateTicks(TICKS_PER_MINUTE * N_MINUTOS);
+    TEST_ASSERT_FALSE(alarmaActivada);
+}
+
+/* Test de alarma,  es pospone n minutos, y no suena hasta pasarse n minutos*/
 void test_alarm_postponed_on(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
-    static const uint8_t N_MINUTOS = 5;                 /* Constante con la cantidad de minutos que se desea posponer */
+    /*Array que con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t N_MINUTOS = 5;
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
@@ -263,11 +307,14 @@ void test_alarm_postponed_on(void) {
     TEST_ASSERT_TRUE(alarmaActivada);
 }
 
-/* Test, la la alarma es pospuesta dos veces, n minutos la primera vez, y m minutos la segunda*/
+/* Test de alarma, se pospone dos veces, n minutos la primera vez, y m minutos la segunda*/
 void test_alarm_postponed_on_twice(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
-    static const uint8_t N_MINUTOS = 5;                 /* Constante con la cantidad de minutos que se desea posponer */
-    static const uint8_t M_MINUTOS = 15;                /* Constante con la cantidad de minutos que se desea posponer */
+    /*Array que con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t N_MINUTOS = 5;
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t M_MINUTOS = 15;
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
@@ -285,10 +332,12 @@ void test_alarm_postponed_on_twice(void) {
     TEST_ASSERT_TRUE(alarmaActivada);
 }
 
-/* Test, la alarma es pospuesta 5 minutos , luego apagada. Y no vuelve a sonar hasta que pasan 23HS 55 minutos */
+/* Test de alarma, suena, se pospone 5 minutos y luego se apaga. No debe volver a sonar hasta que pasan 23HS 55 minutos */
 void test_alarm_postponed_and_shut(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
-    static const uint8_t N_MINUTOS = 5;                 /* Constante con la cantidad de minutos que se desea posponer */
+    /*Array que con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t N_MINUTOS = 5;
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
@@ -307,20 +356,14 @@ void test_alarm_postponed_and_shut(void) {
     TEST_ASSERT_TRUE(alarmaActivada);
 }
 
-/* Test, la alarma es pospuesta pero no estaba sonando (no deberia ocurrir nada)*/
-void test_alarm_postponed_off(void) {
-    static const uint8_t N_MINUTOS = 5; /* Constante con la cantidad de minutos que se desea posponer */
-
-    postponeAlarm(reloj, TIME_SIZE, TIME_SIZE);
-    simulateTicks(TICKS_PER_MINUTE * N_MINUTOS);
-    TEST_ASSERT_FALSE(alarmaActivada);
-}
-
-/* Test, la alarma se pospone para un horario posterior a las 23:59 */
+/* Test de alarma, se pospone para un horario posterior a las 23:59 */
 void test_alarm_postponed_midnight(void) {
-    static const uint8_t HORA[] = {2, 3, 5, 0, 0, 0};   /*Array que representa la alarma esperada*/
-    static const uint8_t ALARMA[] = {2, 3, 5, 4, 0, 0}; /*Array que representa la alarma esperada*/
-    static const uint8_t N_MINUTOS = 5;                 /* Constante con la cantidad de minutos que se desea posponer */
+    /*Array con la hora a setear*/
+    static const uint8_t HORA[] = {2, 3, 5, 0, 0, 0};
+    /*Array con la alarma esperada*/
+    static const uint8_t ALARMA[] = {2, 3, 5, 4, 0, 0};
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t N_MINUTOS = 5;
 
     setTime(reloj, HORA, TIME_SIZE);
     setAlarm(reloj, ALARMA, TIME_SIZE);
@@ -338,8 +381,10 @@ void test_alarm_postponed_midnight(void) {
 
 /* Test, la alarma se debe posponer 5 minutos DESDE que se pospone */
 void test_alarm_postponed_5_min(void) {
-    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0}; /*Array que representa la alarma esperada*/
-    static const uint8_t N_MINUTOS = 5;                 /* Constante con la cantidad de minutos que se desea posponer */
+    /*Array con la alarma esperada*/
+    static const uint8_t ALARMA[] = {1, 2, 4, 0, 0, 0};
+    /* Constante con la cantidad de minutos que se desea posponer */
+    static const uint8_t N_MINUTOS = 5;
 
     setAlarm(reloj, ALARMA, TIME_SIZE);
 
